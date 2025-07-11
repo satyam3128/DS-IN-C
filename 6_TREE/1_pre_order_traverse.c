bdf2b0a70ec5a2cfa3f801ba3618b826b5199eec
Satyam3128 <satyam2005suman@gmail.com>
2025-07-11 20:15:33 +0530

    commit change

--- 6_TREE/1_pre_order_traverse.c
+++ 6_TREE/1_pre_order_traverse.c
@@ -1,4 +1,4 @@
-#include<stdlib.h>
+ #include<stdlib.h>
 #include <stdio.h>
 struct node{
     struct node *left;
---
+++ 7_Revision/evaluate.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a = 3+5*6/2^4;
+    printf("%d",a);
+
+    return 0;
+}
